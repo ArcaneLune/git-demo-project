866691fd736182cda08e6826e35450b2e26805fe
黄煜昊 <3359429309@qq.com>
2026-04-03 23:42:12 +0800

    feat: 为用户类添加getter和setter方法

--- src/User.java
+++ src/User.java
@@ -1,3 +1,17 @@
 class User{
-    int age;
+    private int age;
+    private int sum;
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void getAge(){
+        return age;
+    }
+    public void setSum(int sum){
+        this.sum = sum;
+    }
+    public void getSum(){
+        return sum;
+    }
+
 }
